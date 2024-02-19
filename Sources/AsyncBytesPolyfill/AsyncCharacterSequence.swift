2dbb9924b609f79385a2c64e17778f0ee7823b10
Gwynne Raskind <gwynne@vapor.codes>
2024-02-19 07:47:55 -0600

    Implement AsyncCharacterSequence and AsyncUnicodeScalarSequence

--- Sources/AsyncBytesPolyfill/AsyncCharacterSequence.swift
+++ Sources/AsyncBytesPolyfill/AsyncCharacterSequence.swift
@@ -2,19 +2,30 @@
 public struct _polyfill_AsyncCharacterSequence<Base>: AsyncSequence
     where Base: AsyncSequence, Base.Element == UInt8
 {
+    let underlying: _polyfill_AsyncUnicodeScalarSequence<Base>
+    
     // See `AsyncSequence.Element`.
     public typealias Element = Character
     
     public struct AsyncIterator: AsyncIteratorProtocol {
+        var remaining: _polyfill_AsyncUnicodeScalarSequence<Base>.AsyncIterator
+        var accumulator = ""
+        
         // See `AsyncIteratorProtocol.next()`.
         public mutating func next() async throws -> Character? {
-            fatalError()
+            while let scalar = try await self.remaining.next() {
+                self.accumulator.unicodeScalars.append(scalar)
+                if self.accumulator.count > 1 {
+                    return self.accumulator.removeFirst()
+                }
+            }
+            return !self.accumulator.isEmpty ? self.accumulator.removeFirst() : nil
         }
     }
 
     // See `AsyncSequence.makeAsyncIterator()`.
     public func makeAsyncIterator() -> Self.AsyncIterator {
-        .init()
+        .init(remaining: self.underlying.makeAsyncIterator())
     }
 }
 
@@ -24,6 +35,6 @@ extension _polyfill_AsyncCharacterSequence.AsyncIterator: Sendable where Base.As
 
 extension AsyncSequence where Self.Element == UInt8 {
     public var _polyfill_characters: _polyfill_AsyncCharacterSequence<Self> {
-        .init()
+        .init(underlying: .init(base: self))
     }
 }
--- Sources/AsyncBytesPolyfill/AsyncUnicodeScalarSequence.swift
+++ Sources/AsyncBytesPolyfill/AsyncUnicodeScalarSequence.swift
@@ -2,19 +2,61 @@
 public struct _polyfill_AsyncUnicodeScalarSequence<Base>: AsyncSequence
     where Base: AsyncSequence, Base.Element == UInt8
 {
+    var base: Base
+    
     // See `AsyncSequence.Element`.
     public typealias Element = UnicodeScalar
     
     public struct AsyncIterator: AsyncIteratorProtocol {
+        var base: Base.AsyncIterator
+        var leftover: UInt8? = nil
+        
+        private func expectedContinuationCountForByte(_ byte: UInt8) -> Int? {
+            if byte & 0b11100000 == 0b11000000      { 1 }
+            else if byte & 0b11110000 == 0b11100000 { 2 }
+            else if byte & 0b11111000 == 0b11110000 { 3 }
+            else if byte & 0b10000000 == 0b00000000 { 0 }
+            else                                    { nil }
+        }
+        
+        private mutating func nextComplexScalar(_ first: UInt8) async rethrows -> UnicodeScalar? {
+            guard let expectedContinuationCount = self.expectedContinuationCountForByte(first) else {
+                return "\u{FFFD}"
+            }
+            
+            var bytes: (UInt8, UInt8, UInt8, UInt8) = (first, 0, 0, 0), numContinuations = 0
+            
+            while numContinuations < expectedContinuationCount, let next = try await self.base.next() {
+                guard UTF8.isContinuation(next) else {
+                    self.leftover = next
+                    break
+                }
+                numContinuations += 1
+                withUnsafeMutableBytes(of: &bytes) { $0[numContinuations] = next }
+            }
+            return withUnsafeBytes(of: &bytes) { String(decoding: $0, as: UTF8.self).unicodeScalars.first }
+        }
+        
         // See `AsyncIteratorProtocol.next()`.
         public mutating func next() async rethrows -> UnicodeScalar? {
-            fatalError()
+            if let leftover = self.leftover {
+                self.leftover = nil
+                return try await self.nextComplexScalar(leftover)
+            } else if let byte = try await self.base.next() {
+                if UTF8.isASCII(byte) {
+                    return UnicodeScalar(byte)
+                } else {
+                    return try await self.nextComplexScalar(byte)
+                }
+            } else {
+                return nil
+            }
         }
     }
 
     // See `AsyncSequence.makeAsyncIterator()`.
     public func makeAsyncIterator() -> Self.AsyncIterator {
-        .init()
+        .init(base: base.makeAsyncIterator())
     }
 }
 
@@ -24,6 +66,6 @@ extension _polyfill_AsyncUnicodeScalarSequence.AsyncIterator: Sendable where Bas
 
 extension AsyncSequence where Self.Element == UInt8 {
     public var _polyfill_unicodeScalars: _polyfill_AsyncUnicodeScalarSequence<Self> {
-        .init()
+        .init(base: self)
     }
 }
